d4c83f59be6a6b2f564cb68f2dd842b6b5597d13
reno-ir <renecas1996@gmail.com>
2025-12-03 13:46:25 -0500

    Rama decimal-a-binario completada

--- src/main/java/com/logica/binaria/Main.java
+++ src/main/java/com/logica/binaria/Main.java
@@ -1,7 +1,10 @@
 package com.logica.binaria;
 
+import com.logica.binaria.services.DecimalToBinary;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // main mínimo: delega completamente a DecimalToBinary sin lógica local
+        System.out.println(DecimalToBinary.decimalToBinaryFromArgs(args));
     }
 }
