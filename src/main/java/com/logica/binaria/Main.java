5490821aaae0a2d764f7a44448ed5f5bec283b6c
reno-ir <renecas1996@gmail.com>
2025-12-06 21:38:36 -0500

    Se creo la clase BinarySubtraction

--- src/main/java/com/logica/binaria/Main.java
+++ src/main/java/com/logica/binaria/Main.java
@@ -1,7 +1,9 @@
 package com.logica.binaria;
 
+import com.logica.binaria.services.BinarySubtraction;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(BinarySubtraction.subtract("101", "11"));
     }
 }
